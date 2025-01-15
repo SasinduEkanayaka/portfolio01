b59b691e303d0b0cf0dabcc275c4852b55e91a45
sasinduekanayaka <it22562760@my.sliit.lk>
2025-01-16 01:17:37 +0530

    add data

--- src/components/Works.js
+++ src/components/Works.js
@@ -9,6 +9,7 @@ import Sidebar from "../assets/hutch.png";
 import Ecommerce from "../assets/ecommerce.png";
 import HOC from "../assets/hoc.png";
 import MusicApp from "../assets/musicapp.png";
+import Center from "../assets/center.png"
 import "./portfolio.css"
 
 
@@ -33,13 +34,20 @@ function Works(index) {
 </a>
       </SwiperSlide>
       <SwiperSlide>
-        <img src={Ecommerce} alt="" />
+        <a href="https://www.figma.com/proto/sr7jicvVKqEGFUp1qo6lL8/Badminton?node-id=4-152&t=XI3UDz9FOi8vZLla-1">
+        <img src={Ecommerce} alt="badminton" /></a>
       </SwiperSlide>
       <SwiperSlide>
-        <img src={MusicApp} alt="" />
+      <a href="https://www.figma.com/proto/zch9coybqR1IJnhTmsp3hl/MAD?node-id=1-2539&starting-point-node-id=1%3A2539&t=UlrZltEO9vs7nfwx-1">
+        <img src={MusicApp} alt="beach" className="music-app-img" /></a>
       </SwiperSlide>
       <SwiperSlide>
-        <img src={HOC} alt="" />
+      <a href="https://mysliit-my.sharepoint.com/:v:/g/personal/it22562760_my_sliit_lk/EdHodJ_XxuVOrRCBHUFtMsgBR423d5o8I1rw2bp1iOWeIg?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=345pwU">
+        <img src={HOC} alt="bashi" /></a>
+      </SwiperSlide>
+      <SwiperSlide>
+      <a href="https://www.figma.com/proto/vBjaqDvySYS1gSQYjdk6oW/service-project?node-id=1-642&p=f&t=LIA30gmhCB024fpr-1&scaling=contain&content-scaling=fixed&page-id=0%3A1">
+        <img src={Center} alt="center" /></a>
       </SwiperSlide>
     </Swiper>
   </div>
